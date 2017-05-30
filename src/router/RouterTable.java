47f7c2f8991980edba01070690a146ded7412893
Luccas Silveira <luccas.carsil@gmail.com>
2017-05-30 18:10:36 -0300

    Finalização da implementação do método responsável pela atualização da tabela de roteamento local.

--- src/router/RouterTable.java
+++ src/router/RouterTable.java
@@ -1,8 +1,8 @@
 package router;
 
 import java.net.InetAddress;
-import java.net.UnknownHostException;
 import java.util.ArrayList;
+import java.util.HashMap;
 
 public class RouterTable {
     /*
@@ -10,29 +10,42 @@ public class RouterTable {
      * A tabela deve possuir: IP destino, métrica e IP de saída.
      */
 
-	private ArrayList<RouterTableRow> routerTable;
+	private HashMap<String, RouterTableRow> routerTable;
 
-    public RouterTable() {}
+    public RouterTable() {
+        this.routerTable = new HashMap<String, RouterTableRow>();
+    }
 
     public void updateTable(String table, InetAddress IPAddress) {
         // Atualize a tabela de rotamento a partir da string recebida
         // System.out.println(IPAddress.getHostAddress() + ": " + table);
 
-        String destinationIP;
+        String destination_ip;
         int metric;
-        String outgoingIP = IPAddress.getHostAddress();
+        String outgoing_ip = IPAddress.getHostAddress();
 
+        // Verifica se a tabela recebida não está vazia
         if (!table.equals("!")) {
             String[] table_rows = table.split("*");
             String[] table_row;
-            RouterTableRow routerTableRow;
+
+            // Percorre a as linhas da tabela recebida
             for (int i = 0; i < table_rows.length; i++) {
                 table_row = table_rows[i].split(";");
-                destinationIP = table_row[0];
+                destination_ip = table_row[0];
                 metric = Integer.parseInt(table_row[1]);
-                routerTableRow = new RouterTableRow(destinationIP, metric, outgoingIP);
-                //@todo Verificar condições de atualização da tabela
-                //this.routerTable.add(routerTableRow);
+
+                // Verfica se o IP de destino da tabela recebida já existe na tabela de roteamento local
+                if (this.routerTable.containsKey(destination_ip)) {
+                    // Se existe, verifica se a metrica é menor que a contida na tabela de roteamento local
+                    if (metric < this.routerTable.get(destination_ip).getMetric()) {
+                        this.routerTable.get(destination_ip).setMetric(metric);
+                        this.routerTable.get(destination_ip).setOutgoingIP(outgoing_ip);
+                    }
+                } else {
+                    // Insere a nova rota na tabela de roteamento local
+                    this.routerTable.put(destination_ip, new RouterTableRow(destination_ip, metric, outgoing_ip));
+                }
             }
         }
     }
--- src/router/RouterTableRow.java
+++ src/router/RouterTableRow.java
@@ -27,4 +27,16 @@ public class RouterTableRow {
         return this.outgoingIP;
     }
 
+    public void setDestinationIP(String destination_ip) {
+        this.destinationIP = destination_ip;
+    }
+
+    public void setMetric(int metric) {
+        this.metric = metric;
+    }
+
+    public void setOutgoingIP(String outgoing_ip) {
+        this.outgoingIP = outgoing_ip;
+    }
+
 }
